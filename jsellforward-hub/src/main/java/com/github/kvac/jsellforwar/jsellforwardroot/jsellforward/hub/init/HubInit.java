580f16fbddd0c4ffb391525f1a9599c6fc689e8b
KVAC <kvacgeorgij@gmail.com>
2020-10-07 04:49:30 +0000

    init

---
+++ jsellforward-hub/src/main/java/com/github/kvac/jsellforwar/jsellforwardroot/jsellforward/hub/init/HubInit.java
@@ -0,0 +1,14 @@
+package com.github.kvac.jsellforwar.jsellforwardroot.jsellforward.hub.init;
+
+public class HubInit {
+
+    public static void main(String[] args) {
+        HubInit hi = new HubInit();
+        hi.initConfigs();
+
+    }
+
+    private void initConfigs() {
+
+    }
+}
