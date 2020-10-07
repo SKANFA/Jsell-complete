b2a0bc9de7a6a28289bb39e702b00bc56bbea491
KVAC <kvacgeorgij@gmail.com>
2020-10-07 07:19:59 +0000

    #1

---
+++ jsellforward-db/src/main/java/com/github/kvac/jsellforwar/jsellforwardroot/jsellforward/db/init/DbInit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.kvac.jsellforwar.jsellforwardroot.jsellforward.db.init;
+
+/**
+ *
+ * @author jdcs_dev
+ */
+public class DbInit {
+
+    public static void main(String[] args) {
+
+    }
+}
